65392e6e0a7335e94134cad8f119a2111f6219fc
vmagen <vmagen@gmail.com>
2021-02-09 20:26:27 +0200

    commit 1

---
+++ src/Elements/EFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaHeart } from 'react-icons/fa';
+
+const EFooter =
+    <footer style={{ textAlign: 'center', marginTop: 100 }}>
+        <small style={{ letterSpacing: 8 }}>
+          !WE{' '} <FaHeart style={{ color: 'red' }} /> {' '}
+          WINE
+        </small>
+      </footer>
+    
+export default EFooter;
---
+++ src/messages.js
@@ -0,0 +1,27 @@
+export default {
+  
+  en: {
+    ProductManagment:'Manage Products',
+    ProfileManagment:'Manage Profile',
+    WineryDetails:'Edit Winery Details',
+    CustomerView:'Customer Display',
+    AddWine:'Add Wine',
+    AddEvent:'Add Event',
+    AddService:'Add Service',
+    ContactUs:'Contact Us',
+    logOut:'Log Out',
+    ChangeLang:'Change to Hebrew'
+  },
+  he: {
+    ProductManagment:'ניהול מוצרים',
+    ProfileManagment:'ניהול פרופיל',
+    WineryDetails:'עריכת פרטי יקב',
+    CustomerView:'תצוגת לקוח',
+    AddWine:'הוספת יין',
+    AddEvent:'הוספת ארוע',
+    AddService:'הוספת שרות',
+    ContactUs:'צור קשר',
+    logOut:'התנתק',
+    ChangeLang: 'החלף לשפה אנגלית'
+  },
+};
