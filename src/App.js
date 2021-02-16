40712230cdff1af1484c466963d85c0d6583789d
RonNavon <ronyhezkel@gmail.com>
2021-02-16 20:56:26 +0200

    2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { IntlProvider } from 'react-intl';
 import Layout from './Layout';
 import messages from './messages';
 import './styles/App.scss';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   const [locale, setLocale] = useState('he');
 
   return (
+    <BrowserRouter>
     <IntlProvider locale={locale} messages={messages[locale]}>
       <Layout setLocale={setLocale} />
     </IntlProvider>
+    </BrowserRouter>
   );
 }
 
