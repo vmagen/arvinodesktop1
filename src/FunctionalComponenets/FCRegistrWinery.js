56db0a35f1b4ff83075b2efc0d1fc572097c89a4
RonNavon <ronyhezkel@gmail.com>
2021-02-28 20:43:56 +0200

    1

--- src/FunctionalComponenets/FCRegistrWinery.js
+++ src/FunctionalComponenets/FCRegistrWinery.js
@@ -34,83 +34,107 @@ export default function FCRegistrWinery() {
         setValues({ ...values, [prop]: event.target.value });
     };
 
+    const getArea = () => {
+        fetch(`http://localhost:54186/api/User/`,
+            {
+                method: 'GET',
+                headers: new Headers({
+                    'Content-Type': 'application/json; charset=UTF-8',
+                    'Accept': 'application/json; charset=UTF-8',
+                })
+            })
+            .then(res => {
+                console.log('res=', res);
+                console.log('res.status', res.status);
+                console.log('res.ok', res.ok);
+                return res.json()
+            })
+            .then(
+                (result) => {
+                    setCheckEmail({ checkEmail, result })
+                    console.log(result)
+                },
+                (error) => {
+                    console.log("err post=", error);
+                });
+    }
+}
 
-
-    return (
-        <div className="registrDiv"
+return (
+    <div className="registrDiv"
         style={{
             backgroundImage: `url(${bcImage})`,
             backgroundSize: "cover",
             height: "100vh"
         }}>
-            <IntlProvider locale={locale} messages={messages[locale]}>
-                <Paper elevation={3} className="registrPaper">
-                    <Table>
-                        <TableRow>
-                            <TableCell>
-                                <ETimeline isBold2={isBold2}/>
-                            </TableCell>
-                            <TableCell>
-                                <Form className="registrForm" onSubmit={handleSubmit}>
-                                    <TextField
-                                        required
-                                        id="standard-required"
-                                        label="שם היקב"
-                                        type="text"
-                                        onChange={handleChange('firstName')}
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                    />
+        <IntlProvider locale={locale} messages={messages[locale]}>
+            <Paper elevation={3} className="registrPaper">
+                <Table>
+                    <TableRow>
+                        <TableCell>
+                            <ETimeline isBold2={isBold2} />
+                        </TableCell>
+                        <TableCell>
+                            <Form className="registrForm" onSubmit={handleSubmit}>
+                                <TextField
+                                    required
+                                    id="standard-required"
+                                    label="שם היקב"
+                                    type="text"
+                                    onChange={handleChange('firstName')}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
 
-                                    <TextField
-                                        required
-                                        id="standard-required"
-                                        label="כתובת של היקב"
-                                        type="text"
-                                        onChange={handleChange('lastName')}
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                    />
-                                    <TextField
-                                        required
-                                        id="standard-number"
-                                        label="טלפון ביקב"
-                                        type="number"
-                                        onChange={handleChange('phone')}
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                    />
-                                    <TextField
-                                        required
-                                        id="standard-required"
-                                        label="אימייל ביקב"
-                                        type="text"
-                                        onChange={handleChange('email')}
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                    />
-                                    <TextField
-                                        required
-                                        id="standard-required"
-                                        label="תיאור קצר על היקב"
-                                        type="text"
-                                        onChange={handleChange('email')}
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                    />
-                                    <hr />
-                                    <Button className="registrButton" variant="outline-secondary" type="submit"> הבא </Button>
-                                </Form>
-                            </TableCell>
-                        </TableRow>
-                    </Table>
-                </Paper>
-            </IntlProvider>
-        </div>
-    )
+                                <TextField
+                                    required
+                                    id="standard-required"
+                                    label="כתובת של היקב"
+                                    type="text"
+                                    onChange={handleChange('lastName')}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <TextField
+                                    required
+                                    id="standard-number"
+                                    label="טלפון ביקב"
+                                    type="number"
+                                    onChange={handleChange('phone')}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <TextField
+                                    required
+                                    id="standard-required"
+                                    label="אימייל ביקב"
+                                    type="text"
+                                    onChange={handleChange('email')}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <TextField
+                                    required
+                                    id="standard-required"
+                                    label="תיאור קצר על היקב"
+                                    type="text"
+                                    onChange={handleChange('email')}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <hr />
+                                <Button className="registrButton" variant="outline-secondary" type="submit"> הבא </Button>
+                            </Form>
+                        </TableCell>
+                    </TableRow>
+                </Table>
+            </Paper>
+        </IntlProvider>
+    </div>
+)
 }
